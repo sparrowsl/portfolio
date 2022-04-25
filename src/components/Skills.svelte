44a8a75eecfe6224c53df9842c9d34dde3dbfe0b
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-04-25 14:24:06 +0000

    feat: add about and skills section for mobile

---
+++ src/components/Skills.svelte
@@ -0,0 +1,42 @@
+<script></script>
+
+<section>
+  <!-- Languages -->
+  <div class="languages">
+    <h3 class="">Languages</h3>
+    <p>HTML, CSS, JavaScript, Python</p>
+  </div>
+  <!--  frameworks -->
+  <div class="frameworks">
+    <h3 class="">Frameworks</h3>
+    <p>UIkit, Bulma, Svelte</p>
+  </div>
+  <!--  tools and platforms -->
+  <div class="tools-platforms">
+    <h3 class="">Tools &amp; Platforms</h3>
+    <p>Git/GitHub, Slack, Discord, Netlify, Heroku, Markdown</p>
+  </div>
+</section>
+
+<style>
+  section{
+    display: grid;
+    gap: 1em;
+  }
+  div{
+    padding: 1.5em 1em;
+    background-color: rgba(255, 255, 255, 0.582);
+    border-radius: 5px;
+  }
+  h3{
+    font-family: "Playfair Display", monospace, serif;
+    font-size: 1.8rem;
+    margin-bottom: .7em;
+  }
+  p{
+    font-size: 1.625rem;
+    font-family: "PT Sans", sans-serif;
+    color: #333;
+    line-height: 1.75;
+  }
+</style>
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,7 +1,40 @@
 <script>
-  import { fly } from "svelte/transition"
+  import { fly } from "svelte/transition";
+  import Skills from "../components/Skills.svelte";
 </script>
 
-<section transition:fly={{ y: 200, duration: 2000 }}>
-  <h1>About</h1>
-</section>
+<article transition:fly={{ y: 200, duration: 2000 }} class="container">
+  <!-- About summary -->
+  <h1>About Me</h1>
+  <section class="about-me">
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+      distinctio veniam quod nostrum. Nostrum officia ad, delectus maiores
+      fugiat non!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus
+      similique sequi id possimus. Eius iste quibusdam corrupti, mollitia neque
+      quod voluptate ab, esse, ullam harum quos molestiae dolores minus. Ullam.
+    </p>
+  </section>
+  <!-- Skills section -->
+  <Skills />
+</article>
+
+<style>
+  h1{
+    font-size: 3rem;
+    text-align: center;
+    color: silver;
+    margin-block: .7em;
+    font-family: "Playfair Display", monospace, serif;
+  }
+  .about-me p {
+    font-size: 1.75rem;
+    line-height: 1.5;
+    font-family: "PT Sans", sans-serif;
+    color: #333;
+    margin-bottom: 1em;
+  }
+</style>
