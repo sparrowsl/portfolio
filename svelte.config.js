833d8cf2a34586e8aa68efcf5d2fcab66cc3127c
Benjamin Thorpe <benithorpe@gmail.com>
2022-06-20 08:27:26 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,11 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+    	prerender: {
+		  default: true
+		},
+	},
 };
 
 export default config;
