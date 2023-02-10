01cf695254077345524d9ef53aaa103d65db04c4
benjithorpe <benthorpe.dev@gmail.com>
2023-02-10 08:43:18 +0000

    updated the background color to navy

--- src/lib/components/shared/Navigation.svelte
+++ src/lib/components/shared/Navigation.svelte
@@ -8,12 +8,12 @@
 	];
 </script>
 
-<nav class="flex min-h-[6em] items-center justify-between">
-	<span class="font-bold">BIT</span>
+<nav class="flex min-h-[10vh] items-center justify-between">
+	<span class="font-bold text-teal-400">BIT</span>
 
 	<div class="flex items-center gap-5">
 		{#each menuLinks as { href, name }}
-			<a {href} class="text-sm">{name}</a>
+			<a {href} class="text-sm hover:text-teal-400">{name}</a>
 		{/each}
 	</div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<section class="flex min-h-[80vh] flex-col justify-center">
-	<span class="mb-5 block">Hi, my name is</span>
-	<h1 class="mb-2 text-5xl font-bold">Marvelous Bend</h1>
-	<h2 class="text-light_slate text-4xl font-bold">I build things on the web.</h2>
-	<p class="my-5 max-w-xl font-normal">
+<section class="flex min-h-[90vh] flex-col justify-center">
+	<span class="mb-5 block text-teal-400">Hello there! I am</span>
+	<h1 class="mb-2 text-5xl font-bold lg:text-7xl">Marvelous Bend</h1>
+	<h2 class="text-light_slate text-4xl font-bold lg:text-6xl">I build things on the web.</h2>
+	<p class="my-5 max-w-xl font-normal lg:max-w-2xl">
 		I am a Software Engineer specializing in building simple, fast responsive and functional
 		softwares that work <i class="font-light">(eg: Websites, Web Apps, Mobile Apps)</i>. Currently,
 		I'm focused on building accessible, fast, smooth and user-friendly mobile apps at
@@ -22,7 +22,9 @@
 			class="text-teal-400">Svelte.js</a
 		>
 	</p>
-	<a href="/" class="block w-fit rounded border border-teal-400 px-4 py-2">Check out my Resume</a>
+	<a href="/" class="block w-fit rounded border border-teal-400 px-4 py-3 text-sm text-teal-400">
+		Check out my Resume
+	</a>
 
 	<!-- I build simple, user-friendly, responsive, fast, and functional websites that work. My aim is to
 	build and contribute to building something useful for myself, the community, and the world as a
