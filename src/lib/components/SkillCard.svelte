8ca0cbf50885f0bcc90a11e707e422b45307e62f
sparrowsl <benthorpe.dev@gmail.com>
2023-04-12 17:39:36 +0000

    refactor: add color to icons

--- src/lib/components/SkillCard.svelte
+++ src/lib/components/SkillCard.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Icon from "@iconify/svelte";
 
-	/** @type {{ icon: string, name: string }} */
+	/** @type {{ icon: string, name: string, color: string }} */
 	export let skill;
 </script>
 
-<figure class="text-center transition-transform hover:(cursor-pointer -translate-y-3)">
-	<Icon icon="{skill.icon}" class="mx-auto text-gray-800 text-5xl" />
+<figure class="text-center transition-transform hover:(cursor-pointer -translate-y-3) ">
+	<Icon icon="{skill.icon}" class="mx-auto text-5xl" color="{skill.color}" />
 	<figcaption class="font-normal mt-2 text-slate_light text-sm tracking-wider capitalize">
 		{skill.name}
 	</figcaption>
