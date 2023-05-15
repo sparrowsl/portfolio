25fc648275e22a004743e577b4617b2f36dfcf7d
sparrowsl <benthorpe.dev@gmail.com>
2023-05-15 17:42:21 +0000

    update autohealth duration

--- src/lib/stores/experiences.js
+++ src/lib/stores/experiences.js
@@ -1,29 +1,31 @@
 export default [
+	// {
+	// 	title: "Software Developer",
+	// 	company: "Orange Digital Center",
+	// 	duration: "May 2023 ── Present",
+	// 	tasks: [
+	// 		"Contribute to building the company's website which raised the awareness of the company",
+	// 	],
+	// },
 	{
 		title: "Software Developer (Intern)",
 		company: "AutoHealth",
-		duration: "January 2023 ── Present",
+		duration: "January 2023 ── May 2023",
 		tasks: [
 			"Contribute to building the company's website which raised the awareness of the company",
 			"Help in data gathering for key core data for the use of LifeBlood.",
 		],
 	},
 	{
-		title: "Fullstack Web Developer (Intern)",
+		title: "Fullstack Web Developer",
 		company: "SoftMagazin",
 		duration: "June 2022 ── Present",
 		tasks: ["Building an internal web app portal to display information."],
 	},
 	{
-		title: "Slack Community Manager",
-		company: "TheNewBoston",
-		duration: "October 2020 ── March 2021",
-		tasks: ["Responsible for answering questions and directing new users to specific channels"],
-	},
-	{
 		title: "Contributing Developer",
 		company: "EasyToGit",
 		duration: "August 2020 ── April 2021",
-		tasks: ["", ""],
+		tasks: [],
 	},
 ];
