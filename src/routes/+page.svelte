75215de4647c61cadace36c76dc57c1559ce9c44
sparrowsl <benthorpe.dev@gmail.com>
2024-04-19 13:14:27 +0000

    refactor: add footer design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import "@unocss/reset/tailwind.css";
 	import "uno.css";
 	import HeroSection from "./Hero.svelte";
+	import Footer from "./Footer.svelte";
 </script>
 
 <main class="bg-[#fafafb] font-opensans min-h-screen">
@@ -9,5 +10,5 @@
 	<!-- <Skills /> -->
 	<!-- <Experience /> -->
 	<!-- <Projects /> -->
-	<!-- <Footer /> -->
+	<Footer />
 </main>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,42 +1,42 @@
-<article class="bg-navy contact">
-	<div class="container flex flex-col mx-auto min-h-50svh py-30 px-4 justify-center md:px-0">
-		<h2 class="font-bold text-center mb-2 tracking-wide text-4xl uppercase">Get In Touch</h2>
-		<h3 class="mx-auto max-w-xl text-sm text-center text-light_slate mb-5 tracking-wider leading-6">
-			I am open for any new opportunities and my inbox is always available if you want to say hi and
-			discuss Open Source, I will try my best to get back to you ASAP!
+<footer class="bg-light_navy">
+	<section class="container flex flex-col mx-auto min-h-50dvh py-20 justify-center">
+		<h2 class="font-bold mb-2 tracking-wide text-(4xl white center) uppercase">Get In Touch</h2>
+		<h3 class="mx-auto max-w-lg text-(sm center white) mb-5 tracking-wider leading-6">
+			I am open for any new opportunities and my inbox is always available if you want to discuss, I
+			will try my best to get back to you ASAP!
 		</h3>
 
 		<a
 			href="mailto:benthorpe.dev@gmail.com"
-			class="border rounded-full mx-auto border-teal py-2 px-10 text-teal-400 hover:bg-light_navy">
-			Get In Touch
+			class="border rounded-full mx-auto border-teal py-2 px-10 text-white">
+			Shoot me a mail
 		</a>
 
 		<!-- Contact Links -->
 		<ul class="flex mt-10 gap-7 justify-center">
-			<li class="transition-transform text-teal-600 hover:(text-teal-300 -translate-y-1)">
-				<a href="mailto:benthorpe.dev@gmail.com" class="">
-					<div class="text-2xl i-simple-icons:gmail" />
+			<li class="transition-transform text-white hover:(text-teal -translate-y-1)">
+				<a href="https://github.com/sparrowsl">
+					<i class="text-2xl i-simple-icons:github" />
 				</a>
 			</li>
 
-			<li class="transition-transform text-teal-600 hover:(text-teal-300 -translate-y-1)">
-				<a href="https://www.linkedin.com/in/benjithorpe/" class="">
-					<div class="text-2xl i-simple-icons:linkedin" />
+			<li class="transition-transform text-white hover:(text-teal -translate-y-1)">
+				<a href="mailto:benthorpe.dev@gmail.com">
+					<i class="text-2xl i-simple-icons:gmail" />
 				</a>
 			</li>
 
-			<li class="transition-transform text-teal-600 hover:(text-teal-300 -translate-y-1)">
-				<a href="https://twitter.com/sparrow_sl" class="">
-					<div class="text-2xl i-simple-icons:twitter" />
+			<li class="transition-transform text-white hover:(text-teal -translate-y-1)">
+				<a href="https://linkedin.com/in/benjithorpe">
+					<i class="text-2xl i-simple-icons:linkedin" />
 				</a>
 			</li>
 
-			<li class="transition-transform text-teal-600 hover:(text-teal-300 -translate-y-1)">
-				<a href="https://github.com/sparrowsl" class="">
-					<div class="text-2xl i-simple-icons:github" />
+			<li class="transition-transform text-white hover:(text-teal -translate-y-1)">
+				<a href="https://twitter.com/sparrow_sl">
+					<i class="text-2xl i-simple-icons:twitter" />
 				</a>
 			</li>
 		</ul>
-	</div>
-</article>
+	</section>
+</footer>
